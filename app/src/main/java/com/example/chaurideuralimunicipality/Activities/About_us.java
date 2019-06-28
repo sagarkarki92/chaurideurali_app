301c1f5bfa7256486a2900e5e47c22de0e1b66c8
sagar <epistero@gmail.com>
2019-06-29 01:04:35 +0545

    splash screen about us string updated

--- app/src/main/java/com/example/chaurideuralimunicipality/Activities/About_us.java
+++ app/src/main/java/com/example/chaurideuralimunicipality/Activities/About_us.java
@@ -3,6 +3,7 @@ package com.example.chaurideuralimunicipality.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.example.chaurideuralimunicipality.R;
 
@@ -17,5 +18,12 @@ public class About_us extends AppCompatActivity {
         //-----------setting up toolbar
         toolbar = findViewById(R.id.about_us_toolbar);
         toolbar.setTitle("About Us");
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
